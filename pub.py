25e53c4ec45e8871c8d999cc01013c74cd8ad697
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-06-23 01:43:42 +0600

    pub py file updated

--- pub.py
+++ pub.py
@@ -1,13 +1,16 @@
 from http import client
 import paho.mqtt.client as paho
 import sys
+import json
 
 cli = paho.Client()
+
+demo = {"bot1" : 10, "bot2" : 15}
 
 
 if cli.connect("36.255.69.54", 1883, 60) != 0:
     print("Unable to connect")
     sys.exit(-1)
 
-cli.publish("test/status", "Hello from mac hehe", 0)
+cli.publish("iort", json.dumps(demo), 0)
 cli.disconnect()
