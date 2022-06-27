233536c15e1622f58e4c9af3e5dae83db4b76107
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-06-28 02:01:50 +0600

    cvline issue solved

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ def distance(x1, y1, x2, y2):
     dist = math.sqrt(math.fabs(x2-x1)**2 + math.fabs(y2-y1)**2)
     return dist
 
-def find_color1(frame): #purple 
+def find_color1(frame): #green 
 
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    hsv_lowerbound = np.array([124, 71, 212])  
-    hsv_upperbound = np.array([179, 255, 255]) 
+    hsv_lowerbound = np.array([56, 179, 47])  
+    hsv_upperbound = np.array([162, 255, 255]) 
     mask = cv2.inRange(hsv_frame, hsv_lowerbound, hsv_upperbound)
     res = cv2.bitwise_and(frame, frame, mask=mask) #filter inplace
     cnts, hir = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -37,11 +37,11 @@ def find_color1(frame): #purple
     else:
         return (700, 700), False 
     
-def find_color3(frame): #orange
+def find_color3(frame): #blue
   
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    hsv_lowerbound =  np.array([0, 111, 224])
-    hsv_upperbound = np.array([29, 255, 255])
+    hsv_lowerbound =  np.array([110, 71, 0])
+    hsv_upperbound = np.array([160, 255, 255])
     mask = cv2.inRange(hsv_frame, hsv_lowerbound, hsv_upperbound)
     res = cv2.bitwise_and(frame, frame, mask=mask)
     cnts, hir = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -59,11 +59,11 @@ def find_color3(frame): #orange
     else:
         return (700, 700), False 
     
-def find_color2(frame): #blue
+def find_color2(frame): #pink
 
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    hsv_lowerbound =  np.array([80, 80, 210])
-    hsv_upperbound = np.array([114, 255, 255])
+    hsv_lowerbound =  np.array([165, 101, 75])
+    hsv_upperbound = np.array([179, 255, 255])
     mask = cv2.inRange(hsv_frame, hsv_lowerbound, hsv_upperbound)
     res = cv2.bitwise_and(frame, frame, mask=mask)
     cnts, hir = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -122,23 +122,23 @@ while True:
             "bot1" : horizontal_distance_one,
             "bot2" : horizontal_distance_two })
         
-        print(data)
+        # print(data)
         # time.sleep(2)
         #draw all 3 lines
         
 
-        cli = paho.Client()
+        # cli = paho.Client()
 
-        if cli.connect("36.255.69.54", 1883, 60) != 0:
-            print("Unable to connect")
-            sys.exit(-1)
+        # if cli.connect("36.255.69.54", 1883, 60) != 0:
+        #     print("Unable to connect")
+        #     sys.exit(-1)
 
-        cli.publish("iort", json.dumps(data), 0)
-        time.sleep(2)
-        cli.disconnect()
+        # cli.publish("iort", json.dumps(data), 0)
+        # time.sleep(2)
+        # cli.disconnect()
 
         cv2.line(copy_frame, (color1_x, color1_y), (color2_x, color2_y), (0, 0, 255), 2)
-        cv2.line(copy_frame, (color3_x, color3_y), (color2_x, color1_y), (0, 0, 255), 2)
+        cv2.line(copy_frame, (color3_x, color3_y), (color2_x, color2_y), (0, 0, 255), 2)
         # cv2.line(copy_frame, (color2_x, color2_y), (color2_x, color1_y), (0, 0, 255), 2)
         
         cv2.imshow('Distance cal', copy_frame)
